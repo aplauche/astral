9157358e2f7f651f8e241070823cb53e0f76780f
Anton Plauche <antonplauche92@gmail.com>
2022-01-02 20:39:41 -0600

    use new dbConnect function

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,16 +1,49 @@
+
+// /lib/dbConnect.js
 import mongoose from 'mongoose'
 
-const dbConnect = () => {
-    if(mongoose.connection.readyState >= 1){
-        return
+/** 
+Source : 
+https://github.com/vercel/next.js/blob/canary/examples/with-mongodb-mongoose/utils/dbConnect.js 
+**/
+
+
+const MONGODB_URI = process.env.DB_URI
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable'
+  )
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect () {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
 
-    mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useFindAndModify: false,
-        // useCreateIndex: true
-    }).then(con => console.log('connected'))
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then(mongoose => {
+      return mongoose
+    })
+  }
+  cached.conn = await cached.promise
+  return cached.conn
 }
 
 export default dbConnect
